a4115a975682707b02f75ff3c95fbc9a7f8aba2f
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-07 12:30:29 +0200

    Added 'Duck' class

---
+++ BirdsList/Duck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdsList
+{
+    internal class Duck : Bird
+    {
+        public int Size { get; set; }
+        public KindOfDuck Kind { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Kind} {Size} cm";
+        }
+    }
+}

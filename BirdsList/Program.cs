6677e0e067a6834b77bbccabfadc3873dd55ecf0
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-07 12:35:49 +0200

    Added 'Main' method

--- BirdsList/Program.cs
+++ BirdsList/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            List<Duck> ducks = new List<Duck>();
+            {
+                new Duck() { Kind = KindOfDuck.krzyżówka, Size = 43 };
+                new Duck() { Kind = KindOfDuck.piżmowa, Size = 45 };
+                new Duck() { Kind = KindOfDuck.nur, Size = 35 };
+                new Duck() { Kind = KindOfDuck.piżmowa, Size = 27 };
+                new Duck() { Kind = KindOfDuck.krzyżówka, Size = 35 };
+                new Duck() { Kind = KindOfDuck.nur, Size = 33 };
+            }
+
+            Bird.FlyAway(ducks, "Minnesota");
         }
     }
 }

319767ee36cb90bc4eb4e218bc2eaf3c847b6dc3
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-07 12:28:01 +0200

    Added 'Bird' class

---
+++ BirdsList/Bird.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdsList
+{
+    internal class Bird
+    {
+        public string Name { get; set; }
+        public virtual void Fly(string destination)
+        {
+            Console.WriteLine($"{this}, leci do {destination}");
+        }
+        public override string ToString()
+        {
+            return $"Ptak {Name}";
+        }
+        public static void FlyAway(List<Bird> flock, string destination)
+        {
+            foreach(Bird bird in flock)
+            {
+                bird.Fly(destination);
+            }
+        }
+    }
+}
